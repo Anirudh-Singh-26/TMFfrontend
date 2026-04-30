09aaaf5b7beced1adeadea2a1ff842ff7b86f46a
Anirudh-Singh-26 <anirudhrathore2003@gmail.com>
2026-04-30 23:28:52 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-// const BASE = import.meta.env.VITE_API_URL + "/api";
-const BASE ="/api";
+const BASE = import.meta.env.VITE_API_URL + "/api";
+// const BASE ="/api";
 
 
 async function request(path, opts = {}) {

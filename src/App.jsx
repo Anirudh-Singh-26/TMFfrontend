2e99261d876f1de2b7ef1832031e1a89c1618d1b
Anirudh-Singh-26 <anirudhrathore2003@gmail.com>
2026-04-30 20:34:21 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import ProjectDetails from "./pages/Project";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Dashboard from "./pages/dashboard";
+import ProjectDetails from "./pages/project";
 import Navbar from "./components/Navbar";
 
 function PrivateRoute({ children }) {
